ad6c8814243f2245f3ca9d9d9a81236e52f6d60e
yvinkesteijn <yevinkesteijn@gmail.com>
2017-06-08 18:02:08 +0200

    results + main

--- code/main_advanced.py
+++ code/main_advanced.py
@@ -5,10 +5,12 @@ protein = "HHPHPHPHPHHHHPPPHPPPHPPPPHPPPHPPPHPHPHHHHHPHPHPHH"
 # score found -21, Shmygelska, 2005, 
 
 def main(protein):
-	print len(protein)
-	protein_object = functions.protein_place(protein)
-	new_protein = simulated_annealing.anneal(protein_object, 'test1', 1)
-	functions.Visualizer2D(new_protein[1], protein, new_protein[0], 'test1')
+	for i in range(20):
+		protein_object = functions.protein_place(protein)
+		new_protein = simulated_annealing.anneal(protein_object, 'test%s' %i, 1)
+		folder_iter.write_csv(new_protein[2], 'result_anneal%s' %i)
+		functions.Visualizer2D(new_protein[1], protein, new_protein[0], 'test%s' %i)
+		print 'hillclimber succes'
 
 if __name__ == '__main__':
 	main(protein)
