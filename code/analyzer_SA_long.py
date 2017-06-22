339227a057ec8dad813e8c451b5588e50bf78e16
ingebieger <inge.bieger@hotmail.com>
2017-06-22 17:03:27 +0200

    Start analysis long

--- code/analyzer_SA_long.py
+++ code/analyzer_SA_long.py
@@ -6,32 +6,47 @@ def main():
 
 def extract_proteins(csv_name):
 	protein_array = []
-	ratio_array = []
+	theo_array = []
+	random_array = []
 	f = open('results/final/%s.csv' % (csv_name),'r')
 	data = csv.reader(f, delimiter=';')
 
 	for row in data:
 		if row[0] != 'protein':
 			protein_array.append(row[0])
+			theo_array.append(row[4])
+			random_array.append(row[2])
 	result_array = []
-	percentage_array = [['protein','percentage','iterations']]
+	percentage_array = [['protein','percentage_highestscore','iterations_highestscore', 'relative_percentage', 'percentage_random', 'percentage_theo']]
 	print protein_array
 	for i in range(len(protein_array)):
 		result = extract_results(protein_array[i])
 		percentage = extract_percentage(result)
+		random_percentage = extract_percentage_divider(random_array[i], result)
+		theo_percentage = extract_percentage_divider(theo_array[i], result)
 		result_array += result
-		percentage_array.append([protein_array[i]] + percentage)
+		percentage_array.append([protein_array[i]] + percentage+random_percentage+theo_percentage)
 	write_csv(result_array, 'SA_protein_overview/SA_score%s' %(csv_name))
 	print percentage_array
 	write_csv(percentage_array, 'SA_protein_overview/SA_percentage%s' %(csv_name))
 
 
+def extract_percentage_divider(divider, result):
+	divider = - float(divider)
+	total = 0
+	for row in result:
+		total += row[2]
+	total = total/(20 * float(divider))
+	return [total]
 
+
+	
 
 def extract_percentage(result):
 	high = 0
 	percentage = 0
 	mean = 0
+	total = 0
 	print result
 	for row in result:
 		if row[2] < high:
@@ -41,12 +56,14 @@ def extract_percentage(result):
 		if row[2] == high:
 			percentage +=1
 			mean += row[3]
+		total += row[2]
 
 	if percentage != 0:
 		mean = mean/percentage
 	
+	total = total/(high*float(20))
 	percentage = int((percentage/float(20))*100)
-	return [percentage, mean]
+	return [percentage, mean, total]
 
 	
 
--- code/enter_delete.py
+++ code/enter_delete.py
@@ -1,9 +1,9 @@
 import csv
 
-f = open('results/SA_protein_overview/SA_percentageanalyze_16_8.csv','r')
+f = open('results/SA_protein_overview/SA_percentageanalyze2_50_25_500.csv','r')
 data = csv.reader(f, delimiter=',')
 
-out = open('results/final/SA_percentageanalyze_16_8.csv','w')
+out = open('results/final/SA_percentageanalyze2_50_25_500.csv','w')
 csv_file = csv.writer(out, delimiter = ',')
 
 for row in data:
