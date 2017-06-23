a6f18f51b5bc7d57cae110ca729cc6618a85f625
yvinkesteijn <yevinkesteijn@gmail.com>
2017-06-23 16:07:45 +0200

    update

--- code/enter_delete.py
+++ code/enter_delete.py
@@ -1,11 +1,18 @@
 import csv
 
-f = open('results/SA_protein_overview/SA_percentageanalyze2_50_25_500.csv','r')
+f = open('results/SA_protein_overview/SA_scoreanalyze_16_8_test.csv','r')
 data = csv.reader(f, delimiter=',')
 
-out = open('results/final/SA_percentageanalyze2_50_25_500.csv','w')
-csv_file = csv.writer(out, delimiter = ',')
-
+row_count = 0
 for row in data:
-	if row != '':
-		csv_file.writerow(row)
+	if row[0] != row[3]:
+		print row[0], row[3]
+	else:
+		print 'same'
+
+	if row_count != 0:
+		if int(row[2]) != abs(int(row[4])):
+			print row[0], row[2], row[4]
+		else:
+			print 'same'
+	row_count += 1
